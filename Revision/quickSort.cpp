a332b222f56acdd6b510113e6c67ee242439aa78
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-06-05 17:29:35 +0530

    revision files deleted

--- Revision/quickSort.cpp
+++ Revision/quickSort.cpp
@@ -1,26 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int partition(vector<int> &arr, int low, int high) {
+// Partition function to rearrange the elements
+int partition(int *arr, int low, int high) {
     int pivot = arr[low];
-    int i = low;
-    int j = high;
+    int i = low, j = high;
 
-    while (i < j) {
+    while (i <= j) {
         while (arr[i] <= pivot && i <= high - 1) {
             i++;
         }
-
         while (arr[j] > pivot && j >= low + 1) {
-            j--;
+            j--; // Decrement j instead of incrementing i
         }
-        if (i < j) swap(arr[i], arr[j]);
+        if (i < j) {
+            swap(arr[i], arr[j]);
+        }
     }
-    swap(arr[low], arr[j]);
+    swap(arr[j], arr[low]);
     return j;
 }
 
-void qs(vector<int> &arr, int low, int high) {
+// Recursive QuickSort function
+void qs(int *arr, int low, int high) {
     if (low < high) {
         int pIndex = partition(arr, low, high);
         qs(arr, low, pIndex - 1);
@@ -28,26 +30,17 @@ void qs(vector<int> &arr, int low, int high) {
     }
 }
 
-vector<int> quickSort(vector<int> arr) {
-    qs(arr, 0, arr.size() - 1);
-    return arr;
-}
-
+// Main function
 int main() {
-    vector<int> arr = {4, 6, 2, 5, 7, 9, 1, 3};
-    int n = arr.size();
-    cout << "Before Using quick Sort: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    int arr[] = {6, 4, 8, 2, 9, 1, 0, 7};
+    int n = 8;
 
-    arr = quickSort(arr);
-    cout << "After Using quick sort: " << "\n";
+    // Perform QuickSort
+    qs(arr, 0, n - 1);
+
+    // Print sorted array
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    cout << "\n";
     return 0;
 }
