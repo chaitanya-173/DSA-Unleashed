85d4aa4eebf6be557617b78b19ad8fc5645cf6ab
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-08-13 20:58:23 +0530

    longest-repeating-subsequence added

--- 25. DP/03_LCS/10-longest-repeating-subsequence.cpp
+++ 25. DP/03_LCS/10-longest-repeating-subsequence.cpp
@@ -9,7 +9,7 @@ int LongestRepeatingSubsequence(string &s) {
   
   for(int i=1; i<=n; i++) {
       for(int j=1; j<=n; j++) {
-          if(s[i-1] == s2[j-1] && i != j) {
+          if(s[i-1] == s2[j-1] && i != j) {  // i != j ensures we are not matching the same character at the same index
               dp[i][j] = 1 + dp[i-1][j-1];
           } else {
               dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
